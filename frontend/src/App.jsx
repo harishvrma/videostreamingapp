2213e11067a575450a5a477ffc12ff543bb699f4
Harish Kumar Verma <80400603+harishvrma@users.noreply.github.com>
2024-07-06 13:39:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,11 @@ import { useRef } from 'react'
 
 function App() {
   const playerRef = useRef(null)
-  const videoLink = "https://videostreamingapp-1.onrender.com"
+  const videoLink = "http://localhost:8000/uploads/courses/11b106da-febe-43a8-9d25-25602e811ab2/index.m3u8"
 
   const videoPlayerOptions = {
     controls: true,
-    responsive: true,
+    responsive: true,z
     fluid: true,
     sources: [
       {
